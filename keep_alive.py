7fa3e4eec15cb9684bf12fc7a226da68e90c4d0f
Vikash89350 <v58184879@gmail.com>
2026-04-23 09:22:34 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -5,10 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Bot is active and running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render jo port dega, ye wahi use karega
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
